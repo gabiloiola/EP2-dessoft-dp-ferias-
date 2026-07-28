c920ca4f1ae3adb6e7587df5a433d6b82f20e2b5
Gabriela Ferreira Loiola <gabrielafl1@al.insper.edu.br>
2026-07-27 22:08:53 -0300

    importa funções e as perguntas

---
+++ jogo.py
@@ -0,0 +1,10 @@
+# Importa funções do EP2 que o jogo utiliza
+from funcoes import (
+    transforma_base,
+    valida_questoes,
+    sorteia_questao_inedita,
+    questao_para_texto,
+    gera_ajuda,
+)
+# Importa o banco de perguntas (lista 'quest') do arquivo perguntas.py
+from perguntas import quest
